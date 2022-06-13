f85fd815bb4c9217e1cf43df635dd23673342230
Yaska-Prog <christianleon442@gmail.com>
2022-06-13 12:55:41 +0700

    Database terbaru + new name webservice

--- database/ezbookingrestaurant.sql
+++ database/ezbookingrestaurant.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Jun 2022 pada 07.16
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Generation Time: Jun 13, 2022 at 07:53 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `account`
+-- Table structure for table `account`
 --
 
 CREATE TABLE `account` (
@@ -38,30 +38,22 @@ CREATE TABLE `account` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `reservasi`
+-- Table structure for table `reservasi`
 --
 
 CREATE TABLE `reservasi` (
   `id` int(11) NOT NULL,
-  `namaPemesan` varchar(45) NOT NULL,
   `tanggalPesanan` date NOT NULL,
   `jumlahMeja` int(11) NOT NULL,
-  `jumlahOrang` int(11) DEFAULT NULL,
-  `idrestaurant` int(11) NOT NULL
+  `jumlahOrang` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `reservasi`
---
-
-INSERT INTO `reservasi` (`id`, `namaPemesan`, `tanggalPesanan`, `jumlahMeja`, `jumlahOrang`, `idrestaurant`) VALUES
-(1, 'Ivan', '2020-12-12', 1, 1, 1),
-(2, 'Christian', '2022-06-10', 2, 2, 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `restaurant`
+-- Table structure for table `restaurant`
 --
 
 CREATE TABLE `restaurant` (
@@ -70,59 +62,66 @@ CREATE TABLE `restaurant` (
   `nama` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
   `alamat` varchar(45) NOT NULL,
-  `avilableTable` int(11) NOT NULL
+  `max_table` int(11) NOT NULL DEFAULT 0,
+  `total_pelanggaan` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `restaurant`
---
-
-INSERT INTO `restaurant` (`id`, `ownerName`, `nama`, `username`, `alamat`, `avilableTable`) VALUES
-(1, 'Christian', 'Pujasera', 'tian', 'Jl.. Blabla', 10);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `account`
+-- Indexes for table `account`
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `reservasi`
+-- Indexes for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_reservasi_restaurant_idx` (`restaurant_id`),
+  ADD KEY `fk_reservasi_account1_idx` (`account_id`);
 
 --
--- Indeks untuk tabel `restaurant`
+-- Indexes for table `restaurant`
 --
 ALTER TABLE `restaurant`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `account`
+-- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `reservasi`
+-- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `restaurant`
+-- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD CONSTRAINT `fk_reservasi_account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_reservasi_restaurant` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
