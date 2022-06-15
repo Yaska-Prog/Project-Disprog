087d0d390a5b476d1ddc8d59ab82055aa734d871
ivanchristiann <107381192+ivanchristiann@users.noreply.github.com>
2022-06-15 14:23:41 +0700

    Summarizing Form

---
+++ EzBookingClient/src/FormRestaurant/FormRestaurant.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package FormRestaurant;
+
+/**
+ *
+ * @author asus
+ */
+public class FormRestaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Check apakah restaurant sudah pernah mendaftar? jika blom tampilkan form input data jika sudah tampilkan form home
+    }
+    
+}
